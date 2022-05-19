14b92d6e6781ee9ad13dc903ba5499f70b163823
구홍지 <kongji9847@gmail.com>
2022-05-19 13:24:21 +0900

    create movie & genre models

--- MTX-pjt-back/movies/models.py
+++ MTX-pjt-back/movies/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+    id = models.IntegerField(primary_key=True)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    poster_path = models.TextField()
+    tmdb_id = models.IntegerField()
+    release_date = models.DateField()
+    popularity = models.FloatField()
+    vote_average = models.FloatField()
+    vote_count = models.IntegerField()
+    genre_ids = models.ManyToManyField(Genre)
