326be71859d8c4325d4f59391307c0b3db8843b4
구홍지 <kongji9847@gmail.com>
2022-05-19 13:49:30 +0900

    create movies models

---
+++ MTX-pjt-back/movies/Serializers/movie.py
@@ -0,0 +1,4 @@
+from rest_framework import serializers
+from ..models import Movie
+
+# 끝말잇기 시작
--- MTX-pjt-back/movies/models.py
+++ MTX-pjt-back/movies/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
 class Genre(models.Model):
     name = models.CharField(max_length=50)
     id = models.IntegerField(primary_key=True)
@@ -15,4 +17,25 @@ class Movie(models.Model):
     popularity = models.FloatField()
     vote_average = models.FloatField()
     vote_count = models.IntegerField()
-    genre_ids = models.ManyToManyField(Genre)
+    genre_ids = models.ManyToManyField(Genre, related_name='movies')
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+
+class Rate(models.Model):
+    score = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
