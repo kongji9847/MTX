2491561858bb317d70d1ccf4b479c7f384af0428
jina jeong <jina092400@gmail.com>
2022-05-19 14:31:01 +0900

    Add accounts/serializers.py

--- MTX-pjt-back/accounts/serializers.py
+++ MTX-pjt-back/accounts/serializers.py
@@ -1,16 +1,32 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from movies.models import Movie
+from movies.models import Rate, Review
 
 class ProfileSerializer(serializers.ModelSerializer):
 
-    # class ReviewSerializer(serializers.ModelSerializer):
+    class ReviewSerializer(serializers.ModelSerializer):
 
-    #     class Meta:
-    #         model = Review
-    #         fields = ('title',)
+        class Meta:
+            model = Review
+            fields = '__all__'
 
-    
+    # 사용자가 작성한 리뷰 목록
+    review_set  = ReviewSerializer(many=True, read_only=True)
+    # 작성한 리뷰 개수
+    review_count = serializers.IntegerField(source='review_set.count', read_only=True)
+
+    # 작성한 댓글 개수
+    comment_count = serializers.IntegerField(source='comment_set.count', read_only=True)
+
+    class RateSerializer(serializers.ModelSerializer):
+
+        class Meta:
+            model = Rate
+            fields = ('score', 'movie_id',)
+
+    rate_set = RateSerializer(many=True, read_only=True)
+    movie_count = serializers.IntegerField(source='rate_set.count', read_only=True)
+
     class Meta:
         model = get_user_model()
-        fields = '__all__'
+        fields = ('username', 'rate_set', 'movie_count', 'review_set', 'review_count', 'comment_count',)
