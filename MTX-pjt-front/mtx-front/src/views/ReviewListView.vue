a6a8bd1a9db4fdb3962d83fc41f4e082752fdc45
jina jeong <jina092400@gmail.com>
2022-05-24 17:05:34 +0900

    Update review view

--- MTX-pjt-front/mtx-front/src/views/ReviewListView.vue
+++ MTX-pjt-front/mtx-front/src/views/ReviewListView.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <h1>Review List</h1>
+  <div class="ReviewList">
+    <!-- <h1>Review List</h1>
     <router-link :to="{ name: 'reviewNew' }">
       <button>Create</button>
     </router-link>
@@ -18,7 +18,68 @@
           댓글 수: {{ review.comment_count }}
         </p>
       </li>
-    </ul>
+    </ul> -->
+    <!-- ------------------------------------------------------ -->
+        <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <h1 class="h3 mb-3 text-gray-800 community-title">Community</h1>
+        <!-- DataTales Example -->
+        <div class="card shadow mb-4">
+            <div class="card-header py-3">
+                <h6 class="m-0 font-weight-bold">영화 잡담 게시판</h6>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                        <thead>
+                            <tr>
+                                <th scope="col">id</th>
+                                <th scope="col">영화</th>
+                                <th scope="col">제목</th>
+                                <th scope="col">작성자</th>
+                                <th scope="col">날짜</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                          <tr v-for="(review, idx) in reviews" :key="idx">
+                            <td>
+                              {{ idx + 1 }}
+                            </td>
+                            <td>
+                              {{ review.movie_title }}
+                            </td>
+                            <td width="40%">
+                              <span class="review-title">
+                                <router-link :to="{ name: 'review', params: {reviewPk: review.id } }">
+                                  {{ review.title }}
+                                </router-link>
+                              </span>
+                              <span class="comment-count">
+                                &nbsp;{{ review.comment_count }}
+                              </span>
+                            </td>
+                            <td>
+                              <span class="review-author">
+                                {{ review.user.username }}
+                                <!-- <router-link :to="{ name: 'profile', params: {username: review.user.username } }">
+                                </router-link> -->
+                              </span>
+                            </td>
+                            <td>
+                              {{ review.updated_at }}
+                            </td>
+                          </tr>
+                        </tbody>
+                    </table>
+                  <router-link :to="{ name: 'reviewNew' }">
+                    <button class="btn review-create-btn">Create</button>
+                  </router-link>
+                </div>
+            </div>
+        </div>
+      </div>
+      <!-- ------------------------------------------------------ -->
   </div>
 </template>
 
@@ -39,6 +100,38 @@ import { mapActions, mapGetters } from 'vuex'
   }
 </script>
 
-<style>
+<style scoped>
 
+  .ReviewList {
+    background-color: #EFEFEF;
+  }
+
+  .community-title {
+    font-family: 'Open Sans', sans-serif;
+    font-weight: 700;
+    font-size: 2.5rem;
+  }
+
+  .table-bordered {
+    font-family: 'NanumSquareAcr';
+  }
+
+  .review-title a {
+    color: #041C2D ;
+    text-decoration-line: none;
+  }
+
+  .review-title:hover {
+  font-weight: bold;
+  }
+
+  .comment-count {
+    font-size: 0.8rem;
+    color: #F8A111;
+  }
+
+  .review-create-btn {
+    color: white;
+    background-color: #F8A111;
+  }
 </style>
